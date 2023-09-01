6c6a059833584415c224418ca27c11bfee477854
beuchehmaxime <maximebeucheh86@gmail.com>
2023-09-01 17:34:31 +0100

    Modified the login and register pages to SM Admin template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-8 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
@@ -39,36 +39,41 @@
                             <div class="col-lg-10">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h2 text-primary">Login</h1>
-                                        <p class="h5 text-gray-900 mb-4">Enter your login details and click on the login button</p>
+                                        <h1 class="h4 text-gray-900 mb-4">Login !</h1>
                                     </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                    <form class="user"  method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                       
+                                        <div class="form-group row">
+                                            <label for="#">Email</label>
+                                            <input type="email" name="email" :value="old('email')" class="form-control form-control-user @error('email')
+                                                is-invalid
+                                            @enderror" id="exampleInputEmail"
+                                                placeholder="Email Address"> <br>
+                                                @error('email')
+                                                    <p class="text-danger">{{$message}}</p>
+                                                @enderror
                                         </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                        <div class="form-group row">
+                                            <label for="#">Password</label>
+                                            <input type="password" name="password" class="form-control form-control-user @error('password')
+                                                is-invalid
+                                            @enderror"
+                                                id="password" placeholder="Password" autocomplete="new-password"> <br>
+                                                @error('password')
+                                                <p class="text-danger">{{$message}}</p>
+                                            @enderror                                         
                                         </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
+                                        
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                      
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="text-danger" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="" href="register.html">Create an Account!</a>
+                                        <a class="small" href="{{route('register')}}">Don't have an account? Register!</a>
                                     </div>
                                 </div>
                             </div>
@@ -76,7 +81,6 @@
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -35,57 +35,67 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                            <div class="col-lg-7">
+                            <div class="col-lg-1"></div>
+                            <div class="col-lg-10">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                                     </div>
-                                    <form class="user">
+                                    <form class="user"  method="POST" action="{{ route('register') }}">
+                                        @csrf
                                         <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                                    placeholder="First Name">
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                                    placeholder="Last Name">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                                placeholder="Email Address">
+                                            <label for="#">Username</label>
+                                            <input id="name" class="form-control form-control-user @error('name')
+                                                is-invalid
+                                            @enderror" type="text" name="name" :value="old('name')" autofocus autocomplete="name">       
+                                            <br>
+                                            @error('name')
+                                                <p class="text-danger">{{$message}}</p>
+                                            @enderror                                   
                                         </div>
                                         <div class="form-group row">
-                                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                                <input type="password" class="form-control form-control-user"
-                                                    id="exampleInputPassword" placeholder="Password">
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <input type="password" class="form-control form-control-user"
-                                                    id="exampleRepeatPassword" placeholder="Repeat Password">
-                                            </div>
+                                            <label for="#">Email</label>
+                                            <input type="email" name="email" :value="old('email')" class="form-control form-control-user @error('email')
+                                                is-invalid
+                                            @enderror" id="exampleInputEmail"
+                                                placeholder="Email Address"> <br>
+                                                @error('email')
+                                                    <p class="text-danger">{{$message}}</p>
+                                                @enderror
                                         </div>
-                                        <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                            Register Account
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Register with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                        </a>
+                                        <div class="form-group row">
+                                            <label for="#">Password</label>
+                                            <input type="password" name="password" class="form-control form-control-user @error('password')
+                                                is-invalid
+                                            @enderror"
+                                                id="password" placeholder="Password" autocomplete="new-password"> <br>
+                                                @error('password')
+                                                <p class="text-danger">{{$message}}</p>
+                                            @enderror                                         
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="#">Confirm Password</label>
+                                            <input type="password" name="password_confirmation" class="form-control form-control-user @error('password_confirmation')
+                                                is-invalid
+                                            @enderror"
+                                                id="password_confirmation" placeholder="Repeat Password" autocomplete="new-password"> <br>
+                                                @error('password_confirmation')
+                                                    <p class="text-danger">{{$message}}</p>
+                                                @enderror
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Register Account</button>
+                                      
                                     </form>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="login.html">Already have an account? Login!</a>
+                                        <a class="small" href="/">Already have an account? Login!</a>
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-lg-1"></div>
                         </div>
                     </div>
                 </div>
